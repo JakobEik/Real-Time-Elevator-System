dd2c53afbcea52586ef8007bcc0e46383d84f9f6
Jakob Eikeland <jakeik@hotmail.com>
2023-03-06 15:14:11 +0100

    core shell fsm

--- elevator/fsm.go
+++ elevator/fsm.go
@@ -1,6 +1,9 @@
 package elevator
 
-import "Project/config"
+import (
+	"Project/config"
+	"Project/driver"
+)
 
 /* Put in config!
 var (
@@ -18,49 +21,72 @@ func Fsm(
 	ch_doOrder <-chan config.Order,
 	ch_newCabCall <-chan config.Order,
 	ch_floorArrival <-chan int,
-	ch_Obstruction <-chan bool,
+	ch_obstruction <-chan bool,
 	ch_stop <-chan bool) {
 
 	elev := InitElev()
 
-	SetDoorOpenLamp(false)
-	SetMotorDirection(MD_Down)
+	driver.SetDoorOpenLamp(false)
+	driver.SetMotorDirection(driver.MD_Down)
 
 	for {
 		floor := <-ch_floorArrival
 		if floor != 0 {
-			SetMotorDirection(MD_Down)
+			driver.SetMotorDirection(driver.MD_Down)
 		} else {
-			SetMotorDirection(MD_Stop)
+			driver.SetMotorDirection(driver.MD_Stop)
 			break
 		}
 	}
 
 	for {
 		select {
-		case order := <-ch_orderChan:
-
-			switch {
-			case elev.Behaviour == ele:
-				if elev.Floor == order.Floor {
-					// Reset doortimer
-				} else {
-					// Set order at this point to "true"
-				}
-			case elev.Behaviour == Idle:
-				// blablabla
-
-			case elev.Behaviour == DoorOpen:
-
-			}
+		case order := <-ch_doOrder:
+			onNewOrderEvent(order)
+		case order := <-ch_newCabCall:
+			onNewOrderEvent(order)
 		case floor := <-ch_floorArrival:
-			elev.Floor = floor
-			switch {
-			case elev.Behaviour == Moving:
-
-			}
+			onFloorArrivalEvent(floor)
+		case stop := <-ch_stop:
+			onStopEvent(stop)
+		case obstruction := <-ch_obstruction:
+			onObstructionEvent(obstruction)
 		}
 
 	}
 
+}
+
+func onNewOrderEvent(order config.Order) {
+	//TODO: IMPLEMENT
+	switch {
+	case elev.Behaviour == ele:
+		if elev.Floor == order.Floor {
+			// Reset doortimer
+		} else {
+			// Set order at this point to "true"
+		}
+	case elev.Behaviour == Idle:
+		// blablabla
+
+	case elev.Behaviour == DoorOpen:
+
+	}
+}
+
+func onFloorArrivalEvent(floor int) {
+	//TODO: IMPLEMENT
+	elev.Floor = floor
+	switch {
+	case elev.Behaviour == Moving:
+
+	}
+}
+
+func onStopEvent(stop bool) {
+	//TODO: IMPLEMENT
+}
+
+func onObstructionEvent(obstruction bool) {
+	//TODO: IMPLEMENT
 }
