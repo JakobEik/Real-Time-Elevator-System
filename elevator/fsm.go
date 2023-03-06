0dea45df785bc4dff3734a12bb8d890d2d6870c0
ErikRognaldsen <erirog2@gmail.com>
2023-03-06 14:54:58 +0100

    fsm

--- elevator/fsm.go
+++ elevator/fsm.go
@@ -1,5 +1,10 @@
 package elevator
 
+import (
+	"Project/config"
+	"fmt"
+)
+
 /* Put in config!
 var (
 	elev         Elevator
@@ -25,6 +30,7 @@ func Fsm(
 	SetDoorOpenLamp(false)
 	SetMotorDirection(MD_Down)
 
+	// Initialize elevator
 	for {
 		floor := <-ch_floorArrival
 		if floor != 0 {
@@ -38,28 +44,56 @@ func Fsm(
 	for {
 		select {
 		case order := <-ch_orderChan:
+			// Function newOrder
 
-			switch {
-			case elev.Behaviour == ele:
-				if elev.Floor == order.Floor {
-					// Reset doortimer
-				} else {
-					// Set order at this point to "true"
-				}
-			case elev.Behaviour == Idle:
-				// blablabla
 
-			case elev.Behaviour == DoorOpen:
 
-			}
+
+
+
+			// switch {
+			// case elev.Behaviour == elev.Behave:
+			// 	if elev.Floor == order.Floor {
+			// 		// Reset doortimer
+			// 	} else {
+			// 		// Set order at this point to "true"
+			// 	}
+			// case elev.Behaviour == Idle:
+			// 	// blablabla
+
+			// case elev.Behaviour == DoorOpen:
+
+			// }
+
+
+
 		case floor := <-ch_floorArrival:
-			elev.Floor = floor
-			switch {
-			case elev.Behaviour == Moving:
-
-			}
+			onFloorArrival(floor, elev)
 		}
-
 	}
 
+}
+
+func onFloorArrival(floor int, elev Elevator){
+	switch {
+	case elev.Behave == config.Moving:
+		if requests_shouldStop(floor) {
+			SetMotorDirection(MD_Stop)
+			SetDoorOpenLamp(true)
+
+			elev.Behave = config.DoorOpen
+		} else {
+			// continue moving
+			elev.Floor = floor
+		}
+	default:
+		// print error
+		fmt.Print("Error: onFloorArrival() called when behaviour is not Moving")
+	}
+}
+
+func newOrder(){
+	// Check if order is at current floor
+	// If not, set order to true
+	// If yes, set door open
 }
