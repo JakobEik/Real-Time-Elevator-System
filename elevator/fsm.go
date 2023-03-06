6a5c9cb4a5d0eae3851b84e23851fc3e6c5bbec7
Jakob Eikeland <jakeik@hotmail.com>
2023-03-06 17:31:10 +0100

    onNewOrderEvent

--- elevator/fsm.go
+++ elevator/fsm.go
@@ -3,6 +3,8 @@ package elevator
 import (
 	"Project/config"
 	"Project/driver"
+	"fmt"
+	"time"
 )
 
 /* Put in config!
@@ -58,17 +60,39 @@ func Fsm(
 }
 
 func onNewOrderEvent(order config.Order, elev ElevatorState) {
-	//TODO: IMPLEMENT
+	floor := order.Floor
+	btn_type := order.Button
+	fmt.Printf("\n\n%s(%d, %s)\n", "New Order Event", floor, btn_type)
+	elevPrint(elev)
+
+	switch elev.behavior {
+	case config.DoorOpen:
+		if shouldClearImmediatly(elev, floor, btn_type) {
+			time.Sleep(time.Second * config.DoorOpenDuration)
+		} else {
+			elev.orders[floor][btn_type] = true
+		}
+	case config.Moving:
+		elev.orders[floor][btn_type] = true
+	case config.Idle:
+		elev.orders[floor][btn_type] = true
+		direction, behavior := chooseElevDirection(elev)
+		elev.direction = direction
+		elev.behavior = behavior
+		switch elev.behavior {
+		case config.DoorOpen:
+			driver.SetDoorOpenLamp(true)
+			time.Sleep(time.Second * config.DoorOpenDuration)
+			clearAtCurrentFloor(elev, floor)
+		case config.Moving:
+			driver.SetMotorDirection(elev.direction)
+		}
+	}
 
 }
 
 func onFloorArrivalEvent(floor int, elev ElevatorState) {
 	//TODO: IMPLEMENT
-	elev.Floor = floor
-	switch {
-	case elev.Behaviour == Moving:
-
-	}
 }
 
 func onStopEvent(stop bool, elev ElevatorState) {
