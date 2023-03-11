0bb9f0564a3db472aa191b022f3abcc0b7785edd
Jakob Eikeland <jakeik@hotmail.com>
2023-03-11 12:54:02 +0100

    distributor

--- config/config.go
+++ config/config.go
@@ -1,45 +1,31 @@
 package config
 
-import "Project/driver"
+import "Project/elevator"
 
 const N_FLOORS = 4
 const N_BUTTONS = 3
+const N_ELEVATORS = 3
 const DoorOpenDuration = 3
 
-type RequestState int
+type Behavior int
+
+const (
+	Idle        Behavior = 0
+	DoorOpen             = 1
+	Moving               = 2
+	Unavailable          = 3
+)
+
+type GlobalState struct {
+	ElevatorID int
+	states     []elevator.ElevatorState
+}
+
+/*type RequestState int
 
 const (
 	None      RequestState = 0
 	NewOrder  RequestState = 1
 	Confirmed RequestState = 2
 	Complete  RequestState = 3
-)
-
-type Behavior int
-
-const (
-	Idle        Behavior = 0
-	DoorOpen    Behavior = 1
-	Moving      Behavior = 2
-	Unavailable Behavior = 3
-)
-
-/*type ButtonType int
-
-const (
-	HallUp   ButtonType = 0
-	HallDown ButtonType = 1
-	Cab      ButtonType = 2
-)
-
-type Order struct {
-	Floor  int
-	Button ButtonType
-}*/
-
-type CostOrder struct {
-	Id         string
-	Cost       int
-	AssignedID string
-	Order 	   driver.ButtonEvent      
-}
+)*/
