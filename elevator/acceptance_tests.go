de156931d9ee3b7496fe6554511f4ba22b6cc673
ErikRognaldsen <erirog2@gmail.com>
2023-04-15 16:17:53 +0200

    acceptance tests + failroutine

--- elevator/acceptance_tests.go
+++ elevator/acceptance_tests.go
@@ -3,6 +3,7 @@ package elevator
 import (
 	c "Project/config"
 	"Project/driver"
+	"Project/failroutine"
 	"fmt"
 	"strconv"
 )
@@ -18,42 +19,58 @@ func AcceptanceTests(state ElevatorState) {
 func stateIsConsistent(state ElevatorState) {
 	if state.Behavior == c.DOOR_OPEN && state.Direction != driver.MD_Stop {
 		fmt.Println(state)
-		panic("MOTOR SHOULD BE STOPPED WHEN DOOR IS OPEN")
+		// panic("MOTOR SHOULD BE STOPPED WHEN DOOR IS OPEN")
+		fmt.Println("MOTOR SHOULD BE STOPPED WHEN DOOR IS OPEN")
+		failroutine.FailRoutine()
 	}
 	if state.Behavior == c.IDLE && !ordersIsEmpty(state) {
 		fmt.Println(state)
-		panic("BEHAVIOR IS IDLE, ORDERS SHOULD BE EMPTY")
+		//panic("BEHAVIOR IS IDLE, ORDERS SHOULD BE EMPTY")
+		fmt.Println("BEHAVIOR IS IDLE, ORDERS SHOULD BE EMPTY")
+		failroutine.FailRoutine()
 	}
 	if state.Behavior == c.MOVING && state.Direction == driver.MD_Stop {
-		panic("ELEVATOR IS MOVING BUT DIRECTION IS STOP")
+		//panic("ELEVATOR IS MOVING BUT DIRECTION IS STOP")
+		fmt.Println("ELEVATOR IS MOVING BUT DIRECTION IS STOP")
+		failroutine.FailRoutine()
 	}
 	if state.Behavior == c.IDLE && state.Direction != driver.MD_Stop {
-		panic("ELEVATOR IS IDLE BUT DIRECTION IS NOT STOP")
+		//panic("ELEVATOR IS IDLE BUT DIRECTION IS NOT STOP")
+		fmt.Println("ELEVATOR IS IDLE BUT DIRECTION IS NOT STOP")
+		failroutine.FailRoutine()
 	}
 
 	if state.Behavior == c.DOOR_OPEN && state.Direction != driver.MD_Stop {
-		panic("DOOR IS OPEN BUT DIRECTION IS NOT STOP")
+		//panic("DOOR IS OPEN BUT DIRECTION IS NOT STOP")
+		fmt.Println("DOOR IS OPEN BUT DIRECTION IS NOT STOP")
+		failroutine.FailRoutine()
 	}
 }
 
 func floorInValidRange(state ElevatorState) {
 	if state.Floor < 0 || state.Floor > c.N_FLOORS {
 		floor := strconv.Itoa(state.Floor)
-		panic("FLOOR OUTSIDE OF VALID RANGE : " + floor)
+		//panic("FLOOR OUTSIDE OF VALID RANGE : " + floor)
+		fmt.Println("FLOOR OUTSIDE OF VALID RANGE : " + floor)
+		failroutine.FailRoutine()
 	}
 }
 
 func directionIsValid(state ElevatorState) {
 	if state.Direction != driver.MD_Up && state.Direction != driver.MD_Down && state.Direction != driver.MD_Stop {
 		dir := strconv.Itoa(int(state.Direction))
-		panic("DIRECTION IS NOT VALID : " + dir)
+		//panic("DIRECTION IS NOT VALID : " + dir)
+		fmt.Println("DIRECTION IS NOT VALID : " + dir)
+		failroutine.FailRoutine()
 	}
 }
 
 func behaviorIsValid(state ElevatorState) {
 	if state.Behavior != c.IDLE && state.Behavior != c.MOVING && state.Behavior != c.DOOR_OPEN {
 		b := strconv.Itoa(int(state.Behavior))
-		panic("BEHAVIOR IS NOT VALID : " + b)
+		//panic("BEHAVIOR IS NOT VALID : " + b)
+		fmt.Println("BEHAVIOR IS NOT VALID : " + b)
+		failroutine.FailRoutine()
 	}
 }
 
@@ -61,24 +78,32 @@ func ordersAreValid(state ElevatorState) {
 	for _, floorOrders := range state.Orders {
 		if len(floorOrders) != c.N_BUTTONS {
 			fmt.Println(state.Orders)
-			panic("MISMATCH BETWEEN ORDER STATE AND N_BUTTONS")
+			//panic("MISMATCH BETWEEN ORDER STATE AND N_BUTTONS")
+			fmt.Println("MISMATCH BETWEEN ORDER STATE AND N_BUTTONS")
+			failroutine.FailRoutine()
 		}
 		for _, order := range floorOrders {
 			if order != true && order != false {
 				fmt.Println(state.Orders)
-				panic("ORDERS SHOULD BE BOOLEAN")
+				//panic("ORDERS SHOULD BE BOOLEAN")
+				fmt.Println("ORDERS SHOULD BE BOOLEAN")
+				failroutine.FailRoutine()
 			}
 		}
 	}
 
 	if state.Orders[0][driver.BT_HallDown] {
 		fmt.Println(state.Orders)
-		panic("INVALID HALL DOWN ORDER AT BOTTOM FLOOR")
+		//panic("INVALID HALL DOWN ORDER AT BOTTOM FLOOR")
+		fmt.Println("INVALID HALL DOWN ORDER AT BOTTOM FLOOR")
+		failroutine.FailRoutine()
 	}
 
 	if state.Orders[c.N_FLOORS-1][driver.BT_HallUp] {
 		fmt.Println(state.Orders)
-		panic("INVALID HALL DOWN ORDER AT BOTTOM FLOOR")
+		//panic("INVALID HALL DOWN ORDER AT BOTTOM FLOOR")
+		fmt.Println("INVALID HALL DOWN ORDER AT BOTTOM FLOOR")
+		failroutine.FailRoutine()
 	}
 
 }
